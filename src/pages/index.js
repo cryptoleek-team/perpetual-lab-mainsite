18c2d54049363a57e1b84abedec7c9ea8a8861c4
yukey <wangyikun1993@gmail.com>
2020-12-24 00:18:59 +1100

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import RoadMap from '../components/RoadMap';
 import Career from '../components/Career';
 import Footer from '../components/Footer';
 import { useTranslation } from 'react-i18next';
-import '../i18n';
+import i18n from '../i18n';
 import Aos from "aos";
 import 'aos/dist/aos.css';
 
@@ -25,18 +25,20 @@ const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   
   
-  const { t,i18n } = useTranslation();
+  const { t } = useTranslation();
   const [ln, setLn] = useState('English');
 
-  const changeLan = (l) => {
+  const changeLan = () => {
     if (ln==='English') {
       setLn('中文');
     } else {
       setLn('English');
     } 
-    i18n.changeLanguage(ln);
-    console.log(l);
   };
+
+  useEffect(() => {
+    i18n.changeLanguage(ln);
+  }, [ln]);
 
   const toggle = () => {
     setIsOpen(!isOpen);
